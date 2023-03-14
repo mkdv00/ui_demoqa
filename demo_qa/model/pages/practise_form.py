acb96b5afde3a12df56f05000fdb72f6cb6a91c5
Maksim Kudaev <kudaev.m@nakukop.com>
2023-03-14 12:12:50 +0300

    refactor: test_submitting_form

--- demo_qa/model/pages/practise_form.py
+++ demo_qa/model/pages/practise_form.py
@@ -17,65 +17,80 @@ class Form:
 
     def validation(self, *args):
         browser.element('.table').all('td').even.should(have.texts(args))
+        return self
 
     def open_page(self):
         browser.open('/automation-practice-form')
         browser.driver.maximize_window()
         remove_ads.remove_junk_ads(browser)
+        return self
 
     def add_name_and_surname(self, user):
         browser.element('#firstName').type(user.first_name)
         browser.element('#lastName').type(user.last_name)
+        return self
 
     def add_contacts(self, user):
         browser.element('#userEmail').type(user.email)
         browser.element('#userNumber').type(user.mobile)
         browser.element('#currentAddress').type(user.address)
+        return self
 
     def add_subject(self, user):
         browser.element('#subjectsInput').type(user.subject).press_enter()
+        return self
 
     def choose_hobby(self, user):
         Checkbox.click_checkbox('[for^=hobbies-checkbox]', user.hobby)
+        return self
 
     def add_state(self, user):
         Dropdown.choose('#state', user.state)
+        return self
 
     def add_city(self, user):
         Dropdown.choose('#city', user.city)
+        return self
 
     def submit(self):
         browser.element('#submit').press_enter()
+        return self
 
     def choose_gender(self, user):
         Radio.radio_button('[name=gender]', user.gender)
+        return self
 
     def select_month(self, user):
         browser.element('.react-datepicker__month-select').click()
         Select.select_by_text('.react-datepicker__month-select', user.birth_month)
+        return self
 
     def select_year(self, user):
         browser.element('.react-datepicker__year-select').click()
         Select.select_by_text('.react-datepicker__year-select', user.birth_year)
+        return self
 
     def select_day(self, user):
         browser.element(f'.react-datepicker__day--0{user.birth_day}').click()
+        return self
 
     def send_file(self):
         path = work_with_path.get_path('resources/photo.jpg')
         browser.element('#uploadPicture').set_value(path)
+        return self
 
     def click(self, locator):
         browser.element(locator).click()
+        return self
 
     def add_birthday(self, user):
         self.click('#dateOfBirthInput')
         self.select_month(user)
         self.select_year(user)
         self.select_day(user)
+        return self
 
     def submit_form(self, user):
-        self.open_page()
         self.add_name_and_surname(user)
         self.add_contacts(user)
         self.choose_gender(user)
--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -5,17 +5,18 @@ from demo_qa.data.user_data import test_user
 from demo_qa.model.pages.practise_form import Form
 
 
-def test_submitting_form():
+def test_submitting_form_successfully():
     allure.dynamic.tag('web')
     allure.dynamic.severity(Severity.NORMAL)
     allure.dynamic.label('owner', 'kudaev.m')
-    allure.dynamic.feature('Проверка отправки формы')
+    allure.dynamic.feature('Successful completion of the form')
 
-    with allure.step('Открываем demoqa'):
+    with allure.step('Open demoqa'):
         form = Form()
+        form.open_page()
 
-    with allure.step('Заполняем данные формы'):
+    with allure.step('Fill form'):
         form.submit_form(test_user)
 
-    with allure.step('Проверяем, что данные формы есть в результирующей таблице'):
+    with allure.step('Check that the form data is in the resulting table'):
         form.validate_form(test_user)
